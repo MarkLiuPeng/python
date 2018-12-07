f5e1b68bd8fa84e31970c8c3648c3ce943aa2070
lp <MarkLiuPeng@yeah.net>
2018-12-07 13:28:42 +0800

    python source code

--- test3.py
+++ test3.py
@@ -7,6 +7,7 @@ x=iter(list)
 print(next(x))
 """
 #在类中创建迭代器
+"""
 class MyClass:
     def __iter__(self):
         self.a=1
@@ -22,3 +23,72 @@ newclass=MyClass()
 test=iter(newclass)
 for i in test:
     print(i)
+"""
+#生成器
+''''
+import sys
+def Function(n):
+    x,y,count=0,1,0
+    while True:
+        if(count>n):
+            return
+        yield x
+        x,y=x,x+y
+        count+=1
+example=Function(20)
+
+while True:
+    try:
+        print(next(example),end=' ')
+    except StopIteration:
+        sys.exit()
+'''
+
+#函数
+"""
+def Function(length,width):
+    print(length*width)
+    #return length*width
+Function(10,20)
+"""
+'''
+def Function(name, sex='man'):  #参
+    print("name:",name)
+    print("sex:",sex)
+    return
+
+            #不可变长度参数
+Function('lp')
+def Function(args,*tuple):
+    print(args)
+    print(tuple)
+Function(1,2,3)
+def Function(args,**dict):
+    print(args)
+    print(dict)
+Function(1,x=1,y=2)
+def Function(x,*,y):
+    print(x+y)
+    return x+y
+Function(1,y=3)
+#匿名函数
+sum=lambda x,y:x+y
+print("sum:",sum(1,2))
+#变量
+def PFunction():
+    num=1
+    def Function():
+        nonlocal num
+        #    global num
+        print(num)
+        num=10
+    Function()
+    print(num)
+PFunction()
+#列表推导式
+var_new=[j*i for i in range(10) for j in range(20)]
+print(var_new)
+login={"name":"lp","sex":"man","age":18}
+for k,v in login.items():
+    print(k,v)
+'''
