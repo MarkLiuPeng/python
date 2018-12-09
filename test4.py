5d20a4803fdbb330ea2ad72dcc57c2a1f6345a9f
lp <MarkLiuPeng@yeah.net>
2018-12-09 18:21:24 +0800

    python source code

--- test4.py
+++ test4.py
@@ -11,6 +11,7 @@ elif str== 'unix':
 else:
     print("you system os other")
     '''
+'''
 class MyClass:
     def __init__(self,name,age):
         self.name=name
@@ -18,6 +19,7 @@ class MyClass:
 
 x=MyClass("lp",18)
 print(x.name,x.age)
+'''
 """
     i=1
     def test(self):
@@ -26,3 +28,44 @@ x=MyClass()
 print(x.i)
 print(x.test());
 """
+'''
+class Test:
+    def prt(example):
+        print(example)
+        print(example.__class__)
+t=Test()
+t.prt()
+'''
+#父类
+class  People:
+    name=''         #类基本属性
+    age=0
+    sex=''
+    __height=0      #私有属性
+    def __init__(self,n,a,s):
+        self.name=n
+        self.age=a
+        self.sex=s
+        #self.__height=h
+    def speak(self):
+        print("%s说：我%d岁和性别%s."%(self.name,self.age,self.sex))
+#子类
+class Student(People):
+    score=0
+    def __init__(self,n,a,s,S):
+        People.__init__(self,n,a,s)   #调用父类的构造函数
+        self.score=S
+    def speak(self):            #调用父类的方法
+        print("%s说：我%d岁和性别%s我的分数是%d."%(self.name,self.age,self.sex,self.score))
+
+
+x=Student("lp",18,"man",100)
+x.speak()
+
+'''
+class Myclass:
+    def __init__(self): #构造方法
+        self.data=[]
+
+x=Myclass()
+'''
